31542c5fd88111a44c78b17172c52f5e3a80b548
Prajwal-Neupane <prajwalneupane.981@gmail.com>
2025-09-01 20:24:32 +0545

    forloop

---
+++ forLoops.py
@@ -0,0 +1,18 @@
+total = 0
+
+
+# for i in range(101):
+#     total = total + i
+#     print(total)
+
+
+# print(total)
+
+
+i = 1;
+while(i <= 100):
+    total = total+i
+    i = i+1;
+
+
+print(total)
--- while.py
+++
@@ -1,7 +0,0 @@
-name = ''
-
-while name != name:
-    # name = input("Enter your name: ")
-    print("Enter your name")
-    name = input()
-print("Thank you")
